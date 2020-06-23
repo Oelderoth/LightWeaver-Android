17b66e25162f7f5b9fa5ddf558c3f24d29e88ccf
Oelderoth <GMZues@gmail.com>
2020-06-23 16:06:33 -0500

    Add first-pass navigation aware FAB

--- app/src/main/java/com/example/lightweaver/moblie/MainActivity.kt
+++ app/src/main/java/com/example/lightweaver/moblie/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.lightweaver.moblie
 
+import android.animation.Animator
+import android.graphics.Color
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavController.OnDestinationChangedListener
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -23,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab_add)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -33,12 +45,10 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            showActiveFloatingActionButton(destination.id)
-        }
-
-        findViewById<FloatingActionButton>(R.id.fab_add)?.setOnClickListener { _ ->
-            navController.navigate(R.id.action_start_create_device)
+        setupFabWithNavigationController(navController, fab, listOf(R.id.nav_devices)) { _, destinationId ->
+            when (destinationId) {
+                R.id.nav_devices -> navController.navigate(R.id.action_start_create_device)
+            }
         }
     }
 
@@ -67,5 +77,54 @@ class MainActivity : AppCompatActivity() {
         //        fab?.show()
         //    }
         //})
+    }
+
+
+    class NavigationAwareFloatingActionButtonListener(private val visibleDestinationIds: Collection<Int>, private val fab: FloatingActionButton): Animator.AnimatorListener, NavController.OnDestinationChangedListener, View.OnClickListener {
+        private var currentDestination: Int? = null
+        private val onClickListeners = mutableListOf<(view: View, destinationId: Int?)->Unit>()
+
+        override fun onAnimationRepeat(a: Animator) = Unit
+        override fun onAnimationEnd(a: Animator) = Unit
+        override fun onAnimationCancel(a: Animator) = Unit
+        override fun onAnimationStart(animator: Animator) {
+            if (!visibleDestinationIds.contains(currentDestination)) {
+                animator.cancel()
+            }
+        }
+
+        override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
+            currentDestination = destination.id
+
+            if (visibleDestinationIds.contains(destination.id)) {
+                fab.show()
+            } else {
+                fab.hide()
+            }
+        }
+
+        fun addOnClickListener(listener: (view: View, destinationId: Int?) -> Unit) {
+            onClickListeners.add(listener)
+        }
+
+        override fun onClick(v: View) {
+            for (listener in onClickListeners) {
+                listener(v, currentDestination)
+            }
+        }
+    }
+
+    private fun setupFabWithNavigationController(navController: NavController, fab: FloatingActionButton, visibleDestinationIds: Collection<Int>) {
+        val listener = NavigationAwareFloatingActionButtonListener(visibleDestinationIds, fab)
+        fab.addOnShowAnimationListener(listener)
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    private fun setupFabWithNavigationController(navController: NavController, fab: FloatingActionButton, visibleDestinationIds: Collection<Int>, onClickListener: (view: View, destinationId: Int?) -> Unit) {
+        val listener = NavigationAwareFloatingActionButtonListener(visibleDestinationIds, fab)
+        listener.addOnClickListener(onClickListener)
+        fab.addOnShowAnimationListener(listener)
+        fab.setOnClickListener(listener)
+        navController.addOnDestinationChangedListener(listener)
     }
 }
